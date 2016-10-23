bf6b5fe0b51116fe60bf257d07e0de43778c38c2
Maxwell Paul Brickner <mpb@localhost.localdomain>
2016-10-23 15:43:14 -0400

    1.1 Version: Added Sitemap, More Content, FAQ, and much more.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-The Animarathon Website
+The Animarathon Website is the official website of Anime In Northwest Ohio and it's convention Animarathon.
+Anime In Northwest Ohio is an official student organization of BGSU, please let us know what you think of our website.
+
+Thank you! ^_^
---
+++ script/structured_data.js
@@ -0,0 +1,30 @@
+{
+ "@context": "http://schema.org",
+ "@type": "Event",
+ "name": "Animarathon XV",
+ "startDate": "2017-03-25T9:00",
+ "doorTime": "2017-03-25T9:00",
+ "endDate": "2017-03-25T23:59",
+ "url": "http://animarathon.com/animarathon.html",
+ "image": "http://animarathon.com/media/media_nonfree/sisters.png",
+ "typicalAgeRange": "Everyone",
+ "location": {
+  "@type": "Place",
+  "sameAs": "https://www.bgsu.edu/bowen-thompson-student-union.html",
+  "name": "The Bowen Thompson Student Union",
+  "Preformer": "Anime In Northwest Ohio",
+  "address": {
+   "streetAddress": "Bowen Thompson Student Union, Bowling Green State University", 
+   "addressLocality": "Bowling Green", 
+   "addressRegion": "OH",
+   "postalCode": "43404",
+   }
+  }
+  "offers": {
+   "price": "10",
+   "priceCurrency": "USD",
+   "availability": "PreOrder"
+  }
+ }
+
+}
