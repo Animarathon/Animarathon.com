bdcd7edf23b0f04e2527f9595ca50ed73f29c22a
Maxwell Paul Brickner <mpb@localhost.localdomain>
2016-10-25 09:40:00 -0400

    1.2 Added a a lot of minor tweeks to each page

--- script/structured_data.js
+++ script/structured_data.js
@@ -8,23 +8,16 @@
  "url": "http://animarathon.com/animarathon.html",
  "image": "http://animarathon.com/media/media_nonfree/sisters.png",
  "typicalAgeRange": "Everyone",
- "location": {
+ "location": [{
   "@type": "Place",
   "sameAs": "https://www.bgsu.edu/bowen-thompson-student-union.html",
   "name": "The Bowen Thompson Student Union",
-  "Preformer": "Anime In Northwest Ohio",
-  "address": {
+  "address": [{
    "streetAddress": "Bowen Thompson Student Union, Bowling Green State University", 
    "addressLocality": "Bowling Green", 
    "addressRegion": "OH",
-   "postalCode": "43404",
-   }
-  }
-  "offers": {
-   "price": "10",
-   "priceCurrency": "USD",
-   "availability": "PreOrder"
-  }
- }
+   "postalCode": "43404"
+   }]
+ }]
 
 }
